2652267df586836f51b761a314745bee93a4e02d
YRREALLYCUTE <1983854984@qq.com>
2020-10-23 11:16:28 +0800

    完成lab1 ex3 bufferpool

--- src/java/simpledb/BufferPool.java
+++ src/java/simpledb/BufferPool.java
@@ -5,6 +5,10 @@ import java.io.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * 缓冲池，存储了最近访问的页。
+ * 用来加快访问速度。读磁盘速度较慢，所以将部分页缓存至内存中
+ * 一个缓冲池中，最多拥有numPages的页，在lab1中，超过该限制，则抛出异常，之后会实现
+ * 过期淘汰策略。
  * BufferPool manages the reading and writing of pages into memory from
  * disk. Access methods call into it to retrieve pages, and it fetches
  * pages from the appropriate location.
@@ -12,20 +16,24 @@ import java.util.concurrent.ConcurrentHashMap;
  * The BufferPool is also responsible for locking;  when a transaction fetches
  * a page, BufferPool checks that the transaction has the appropriate
  * locks to read/write the page.
- * 
- * @Threadsafe, all fields are final
+ * 注意线程安全, final关键字标识的变量是读线程安全的
+ * pages肯定是需要写入的，此处仍选用ConcurrentHashMap
+ * @Threadsafe all fields are final
  */
 public class BufferPool {
     /** Bytes per page, including header. */
     private static final int DEFAULT_PAGE_SIZE = 4096;
 
     private static int pageSize = DEFAULT_PAGE_SIZE;
-    
+
+    private  ConcurrentHashMap<PageId, Page> pages;
+
     /** Default number of pages passed to the constructor. This is used by
     other classes. BufferPool should use the numPages argument to the
     constructor instead. */
     public static final int DEFAULT_PAGES = 50;
 
+    private final int maxPages;
     /**
      * Creates a BufferPool that caches up to numPages pages.
      *
@@ -33,27 +41,41 @@ public class BufferPool {
      */
     public BufferPool(int numPages) {
         // some code goes here
+        pages = new ConcurrentHashMap<>();
+        maxPages = numPages;
     }
-    
+
     public static int getPageSize() {
       return pageSize;
     }
-    
+
     // THIS FUNCTION SHOULD ONLY BE USED FOR TESTING!!
     public static void setPageSize(int pageSize) {
     	BufferPool.pageSize = pageSize;
     }
-    
+
     // THIS FUNCTION SHOULD ONLY BE USED FOR TESTING!!
     public static void resetPageSize() {
     	BufferPool.pageSize = DEFAULT_PAGE_SIZE;
     }
 
     /**
+     * 检索具有关联权限的指定页。将获取锁，如果该锁被另一事务持有，则可能会阻塞
      * Retrieve the specified page with the associated permissions.
      * Will acquire a lock and may block if that lock is held by another
      * transaction.
      * <p>
+     *     检索页需要在缓冲池中进行查找，如果在缓冲池中出现了，则需要返回该页。
+     *     如果没有出现，则需要被加入缓冲池中，然后返回。
+     *     如果当前缓冲池没有足够的空间，那么需要移除一个页，然后将新的页插入其位置
+     *
+     *     lab1
+     *     只需要完成getPage()函数，并且不需要完成锁、事务、淘汰机制等。。所以此函数现在可以简单的写成根据PageId找page
+     *     目前是在seqscan的时候会涉及到该函数。根据要求，每次查找一个页
+     *        - 如果查到则返回
+     *        - 如果没有查到，但是有空间，则插入缓冲池，并返回
+     *        - 如果没有查到，且缓冲池没有空间，则直接抛出异常 （后续实现淘汰策略）
+     *
      * The retrieved page should be looked up in the buffer pool.  If it
      * is present, it should be returned.  If it is not present, it should
      * be added to the buffer pool and returned.  If there is insufficient
@@ -67,7 +89,14 @@ public class BufferPool {
     public Page getPage(TransactionId tid, PageId pid, Permissions perm)
         throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+        if(pages.get(pid) != null)
+            return pages.get(pid);
+        if(pages.size() >= maxPages)
+            throw new DbException("lab1--缓冲池空间不足");
+        Page page = Database.getCatalog().getDatabaseFile(pid.getTableId()).readPage(pid);
+        pages.put(pid, page);
+
+        return page;
     }
 
     /**
@@ -116,14 +145,14 @@ public class BufferPool {
 
     /**
      * Add a tuple to the specified table on behalf of transaction tid.  Will
-     * acquire a write lock on the page the tuple is added to and any other 
-     * pages that are updated (Lock acquisition is not needed for lab2). 
+     * acquire a write lock on the page the tuple is added to and any other
+     * pages that are updated (Lock acquisition is not needed for lab2).
      * May block if the lock(s) cannot be acquired.
-     * 
+     *
      * Marks any pages that were dirtied by the operation as dirty by calling
-     * their markDirty bit, and adds versions of any pages that have 
-     * been dirtied to the cache (replacing any existing versions of those pages) so 
-     * that future requests see up-to-date pages. 
+     * their markDirty bit, and adds versions of any pages that have
+     * been dirtied to the cache (replacing any existing versions of those pages) so
+     * that future requests see up-to-date pages.
      *
      * @param tid the transaction adding the tuple
      * @param tableId the table to add the tuple to
@@ -141,9 +170,9 @@ public class BufferPool {
      * other pages that are updated. May block if the lock(s) cannot be acquired.
      *
      * Marks any pages that were dirtied by the operation as dirty by calling
-     * their markDirty bit, and adds versions of any pages that have 
-     * been dirtied to the cache (replacing any existing versions of those pages) so 
-     * that future requests see up-to-date pages. 
+     * their markDirty bit, and adds versions of any pages that have
+     * been dirtied to the cache (replacing any existing versions of those pages) so
+     * that future requests see up-to-date pages.
      *
      * @param tid the transaction deleting the tuple.
      * @param t the tuple to delete
@@ -169,7 +198,7 @@ public class BufferPool {
         Needed by the recovery manager to ensure that the
         buffer pool doesn't keep a rolled back page in its
         cache.
-        
+
         Also used by B+ tree files to ensure that deleted pages
         are removed from the cache so they can be reused safely
     */
